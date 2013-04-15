eb3833bcfbd27695800f2d0750301b34923a3b51
Simen A.W. Olsen <cobraz@cobraz.no>
2013-04-15 17:24:13 +0200

    .. no words..

--- start.php
+++ start.php
@@ -4,7 +4,7 @@ include(path('bundle')."laravel-payex/payex/payex.php");
 if(file_exists(path('app')."/config/payex.php")){
 	PayEx::setConfigFile(path('app')."/config/payex.php");
 } else {
-	PayEx::setConfigFile(path('bundles')."laravel-payex/config/payex.php");
+	PayEx::setConfigFile(path('bundle')."laravel-payex/config/payex.php");
 }
 
 ?>
