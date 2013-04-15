e67d28ad7b057c9adb3c0b33820bba3585e8effa
Simen A.W. Olsen <cobraz@cobraz.no>
2013-04-15 17:23:15 +0200

    Another stupid mistake fixed!

--- start.php
+++ start.php
@@ -1,8 +1,8 @@
 <?php
 
 include(path('bundle')."laravel-payex/payex/payex.php");
-if(file_exists(path('application')."/config/payex.php")){
-	PayEx::setConfigFile(path('application')."/config/payex.php");
+if(file_exists(path('app')."/config/payex.php")){
+	PayEx::setConfigFile(path('app')."/config/payex.php");
 } else {
 	PayEx::setConfigFile(path('bundles')."laravel-payex/config/payex.php");
 }
